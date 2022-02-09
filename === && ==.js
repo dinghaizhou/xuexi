011cdc45126416eb19dbf7cd5e5c7daf0c1180b8
dinghaizhou <dinghaizhou@baidu.com>
2022-02-09 14:50:11 +0800

    init

--- === && ==.js
+++
@@ -1,9 +0,0 @@
-/**
- * NaN === NaN                      falsed *
- * null === null                    true *
- * undefined === undefined          true *
- * {} === {}                        false
- * 
- * 
- * 
- */
